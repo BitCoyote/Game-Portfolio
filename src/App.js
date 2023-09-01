f21ca7d29be276fff06d8c5b87909659e3edf139
BitCoyote <fanjoey1210@gmail.com>
2023-09-01 10:53:02 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="name-text">
+        Daniel Anthony Mix
+      </div>
+
+      <div className="box-container">
+        <div className="box">
+          <img src="./assets/zoran.jpg" />
+        </div>
+        <div className="box">
+          <img src="./assets/hg.jpg" />
+        </div>
+        <div className="box">
+          <img src="./assets/creed.jpg" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
